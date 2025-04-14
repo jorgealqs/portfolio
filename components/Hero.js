3631c05305383161463c96797c2449b89577e855
Jorge Alberto Quiroz Sierra <joralquisi@hotmail.com>
2025-04-14 10:44:03 -0500

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -12,7 +12,7 @@ export class Hero extends Component {
                     Soy un ingeniero con sólida experiencia en desarrollo backend, especializado en <span class="font-semibold text-yellow-500">Python</span> 🐍 y experto en la implementación y personalización de <span class="font-semibold text-yellow-500">Odoo ERP</span> 📦 para automatizar y optimizar procesos empresariales. Me apasiona crear soluciones inteligentes que conecten la tecnología con la productividad real de las empresas. También tengo conocimientos en <span class="font-semibold text-yellow-500">PostgreSQL, Docker</span> y <span class="font-semibold text-yellow-500">despliegue en la nube</span>, lo que me permite entregar proyectos robustos, escalables y bien documentados.
                 </p>
                 <div class="flex flex-col md:flex-row items-center gap-4">
-                    <a href="/portfolio/static/files/Jorge.pdf"
+                    <a href="/portfolio/files/Jorge.pdf"
                         class="bg-yellow-400 text-black px-6 py-2 rounded-full font-semibold hover:bg-yellow-300 transition shadow-md">
                         Descargar CV
                     </a>
@@ -26,7 +26,7 @@ export class Hero extends Component {
             <!-- Foto -->
             <div class="md:w-1/2 flex justify-center mb-10 md:mb-0" data-aos="fade-left">
                 <img
-                    src="/portfolio/static/image/foto.jpg"
+                    src="/portfolio/image/foto.jpg"
                     alt="Foto de perfil"
                     class="w-64 h-64 rounded-full border-4 border-yellow-400 shadow-xl object-cover"
                 />
